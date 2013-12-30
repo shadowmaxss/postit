91c5c5808d9c0d8ac5c3a9522090b8d71004a608
Max Sperando <shadowmaxss@gmail.com>
2013-12-29 22:33:10 -0600

    added categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131230032606) do
+ActiveRecord::Schema.define(version: 20131230041202) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comments", force: true do |t|
     t.text     "body"
     t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "post_categories", force: true do |t|
+    t.integer  "category_id"
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
