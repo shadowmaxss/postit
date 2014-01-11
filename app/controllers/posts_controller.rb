26661b19bca77f08d6a4fb04eee7f893a1db6e39
Max Sperando <shadowmaxss@gmail.com>
2014-01-11 12:25:16 -0600

    added before action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update]
+
   def index
   	@posts = Post.all
   end
 
-  def show
-  	@post = Post.find(params[:id])
-  end
+  def show; end
 
   def new
   	@post = Post.new
@@ -23,14 +23,9 @@ class PostsController < ApplicationController
   	end
   end
 
-  def edit
-  	@post = Post.find(params[:id])
-
-  end
+  def edit; end
 
   def update
-  	@post = Post.find(params[:id])
-
   	if @post.update(post_params)
   		flash[:notice] = "This post was updated."
   		redirect_to post_path(@post)
@@ -45,4 +40,7 @@ class PostsController < ApplicationController
   	params.require(:post).permit(:title, :url, :description)
   end
 
+  def set_post
+  	@post = Post.find(params[:id])
+  end
 end
