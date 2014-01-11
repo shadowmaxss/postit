7b07eb73285b942118b05140b3c5a2e4275f21a0
Max Sperando <shadowmaxss@gmail.com>
2014-01-11 12:13:40 -0600

    added edit post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,9 +24,19 @@ class PostsController < ApplicationController
   end
 
   def edit
+  	@post = Post.find(params[:id])
+
   end
 
   def update
+  	@post = Post.find(params[:id])
+
+  	if @post.update(post_params)
+  		flash[:notice] = "This post was updated."
+  		redirect_to post_path(@post)
+  	else
+  		render :edit
+  	end
   end
 
   private
