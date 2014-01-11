9a4653bab0ba93c012d40b36cc5ad8858f878c06
Max Sperando <shadowmaxss@gmail.com>
2014-01-11 14:11:36 -0600

    Categories fixed up

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,25 @@
+class CategoriesController < ApplicationController
+	def new
+		@category = Category.new
+	end
+
+	def create
+		@category = Category.new(category_params)
+
+		if @category.save
+			flash[:notice] = "Category has bee created."
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	def show
+		@category =	Category.find(params[:id])
+	end
+
+	def category_params
+		params.require(:category).permit(:name)
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
   resources :posts, except: [:delete]
+  resources :categories, only: [:new, :create, :show]
 end
