ea98a2dbffe5cd885b14b4fb27d7c7f8d61e3df0
Max Sperando <shadowmaxss@gmail.com>
2014-01-11 11:37:28 -0600

    Created new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,19 @@ class PostsController < ApplicationController
   end
 
   def new
+  	@post = Post.new
   end
 
   def create
+  	@post = Post.new(post_params)
+  	@post.creator = User.first #TODO: change once we have authentication
+
+  	if @post.save
+  		flash[:notice] = "Your post was created."
+  		redirect_to posts_path
+  	else
+  		render :new
+  	end
   end
 
   def edit
@@ -18,5 +28,11 @@ class PostsController < ApplicationController
 
   def update
   end
-  
+
+  private
+
+  def post_params
+  	params.require(:post).permit(:title, :url, :description)
+  end
+
 end
