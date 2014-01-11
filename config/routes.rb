54e30a171c338469bc06fafcf6e8a6b53cb8c8e3
Max Sperando <shadowmaxss@gmail.com>
2014-01-11 15:24:57 -0600

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
-  resources :posts, except: [:delete]
+  resources :posts, except: [:delete] do
+  	resources :comments, only: [:create]
+  end
   resources :categories, only: [:new, :create, :show]
 end
